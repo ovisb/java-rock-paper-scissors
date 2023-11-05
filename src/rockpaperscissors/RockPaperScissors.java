c7fb2c85edc5930f1b2fac5b7f4f9e5f8a8b09f1
Ovidiu <ovidiu.sbangu@gmail.com>
2023-11-05 16:08:06 +0200

    Completed stage 5

--- src/rockpaperscissors/RockPaperScissors.java
+++ src/rockpaperscissors/RockPaperScissors.java
@@ -1,7 +1,5 @@
 package rockpaperscissors;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,90 +13,143 @@ class RockPaperScissors {
         return scanner.next();
     }
 
-    private static final String SCORE_FILE_NAME = "rating.txt";
-
-    private boolean isInputValid(String userInput) {
-        boolean isValid = false;
-
-        try {
-            GameChoices userChoice = GameChoices.valueOf(userInput.toUpperCase());
-
-            for (GameChoices enumChoice: GameChoices.values()) {
-                if (enumChoice == userChoice ) {
-                    isValid = true;
-                    break;
-                }
+    /**
+     * Validate user input
+     * @param allChoices Initial list containing all the games choices
+     * @param userInput User input
+     * @return true if valid else false
+     */
+    private boolean isInputValid(String[] allChoices, String userInput) {
+        for (String choice: allChoices) {
+            if (userInput.equals(choice)) {
+                return true;
             }
-        } catch (IllegalArgumentException ignored) {
-            System.out.println("Invalid input");
         }
-
-        return isValid;
+        System.out.println("Invalid input");
+        return false;
     }
 
-    private String findWhoBeatsChoice(GameChoices choice) {
-        return choice.getLosesAgainst();
+    /**
+     * Return the options which beat the User's option
+     * First half of the array are the options who beat the user, while second half are the one he loses to
+     * @param finalChoices Final list containing all winner / losing options
+     * @return first half of winner options
+     */
+    private String[] getWinners(String[] finalChoices) {
+        int mid = finalChoices.length / 2;
+        String[] winners = new String[mid];
+
+        System.arraycopy(finalChoices, 0, winners, 0, mid);
+        return winners;
     }
 
-    private String getComputerChoice() {
-        GameChoices[] availableChoices = GameChoices.values();
-        int randomInt = random.nextInt(availableChoices.length);
-        return availableChoices[randomInt].name().toLowerCase();
+    /**
+     * Get computer choice
+     * @param allChoices Initial list containing all the games choices
+     * @return Computer choice
+     */
+    private String getComputerChoice(String[] allChoices) {
+        int randomInt = random.nextInt(allChoices.length);
+        return allChoices[randomInt];
     }
 
-    private void checkWinner(String userInput, User user) {
-        String computerChoice = getComputerChoice();
+    /**
+     * Find who won or if it's a draw
+     * @param allChoices Initial list containing all the games choices
+     * @param finalChoices Final list containing all winner / losing options
+     * @param userInput User input
+     * @param user User object
+     */
+    private void checkWinner(String[] allChoices, String[] finalChoices, String userInput, User user) {
+        String computerChoice = getComputerChoice(allChoices);
 
-        GameChoices userChoice = GameChoices.valueOf(userInput.toUpperCase());
-        GameChoices compChoice = GameChoices.valueOf(computerChoice.toUpperCase());
+        String[] winsAgainstUser = getWinners(finalChoices);
 
-        String whoBeatsUser = findWhoBeatsChoice(userChoice);
-        String whoBeatsComp = findWhoBeatsChoice(compChoice);
-
-        if (userInput.equals(whoBeatsComp)) {
-            System.out.printf("Well done. The computer chose %s and failed%n", computerChoice);
-            user.increaseScore(POINTS_WIN);
-        } else if (computerChoice.equals(whoBeatsUser)) {
-            System.out.printf("Sorry, but the computer chose %s%n", computerChoice);
-        } else {
+        if (userInput.equals(computerChoice)) {
             System.out.printf("There is a draw (%s)%n", userInput);
             user.increaseScore(POINTS_DRAW);
+            return;
         }
-    }
 
-    private File readFile() {
-        return new File(SCORE_FILE_NAME);
-    }
-
-    private void setScoreFromFile(User user) throws FileNotFoundException {
-        try (Scanner sc = new Scanner(readFile())) {
-            while (sc.hasNext()) {
-                String[] userData = sc.nextLine().split(" ");
-                String playerName = userData[0];
-                int playerScore = Integer.parseInt(userData[1]);
-
-                if (user.getName().equals(playerName)) {
-                    user.setScore(playerScore);
-                    return;
-                }
+        for (String s: winsAgainstUser) {
+            if (s.equals(computerChoice)) {
+                System.out.printf("Sorry, but the computer chose %s%n", computerChoice);
+                return;
             }
         }
+
+        System.out.printf("Well done. The computer chose %s and failed%n", computerChoice);
+        user.increaseScore(POINTS_WIN);
     }
 
+    /**
+     * Create user
+     * @return user object
+     */
     private User createUser() {
         System.out.print("Enter your name: ");
         return new User(scanner.next());
     }
 
+    /**
+     * Return index location of the user's choice
+     * @param allChoices Initial list containing all the games choices
+     * @param choice User choice
+     * @return index
+     */
+    private int getChoiceIndex(String[] allChoices, String choice) {
+        for (int i = 0; i < allChoices.length; i++) {
+            if (choice.equals(allChoices[i])) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Get all the options excluding the user choice
+     * Return a new list counting with the item that follows the user choice till the very end of the list
+     * Then also add the remaining items starting from the beginning of the list
+     * @param allChoices Initial list containing all the games choices
+     * @param userChoice User choice
+     * @return Array of the options described above
+     */
+    private String[] optionsWithoutChoice(String[] allChoices, String userChoice) {
+        // get location
+        int choiceIdx = getChoiceIndex(allChoices, userChoice);
+        String[] option = new String[allChoices.length - 1];
+
+        // get all options that follows the user choice
+        int i = 0;
+        while (choiceIdx != allChoices.length - 1) {
+            option[i++] = allChoices[++choiceIdx];
+        }
+
+        // get remaining choices starting from the beginning
+        int j = 0;
+        while (!allChoices[j].equals(userChoice)) {
+            option[i++] = allChoices[j++];
+        }
+
+        return option;
+    }
+
+    /**
+     * Main game menu
+     */
     public void menu() {
         User user = createUser();
         System.out.printf("Hello, %s%n", user.getName());
 
-        try {
-            setScoreFromFile(user);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found " + e.getMessage());
+        Scanner sc = new Scanner(System.in);
+        String gameChoices = sc.nextLine();
+
+        if (gameChoices.isEmpty()) {
+            gameChoices = "rock,paper,scissors";
         }
+        System.out.println("Okay, let's start");
+        String[] allChoices = gameChoices.split(",");
+        String[] finalChoices;
 
         loop: while (true) {
             String userInput = getInput();
@@ -112,10 +163,11 @@ class RockPaperScissors {
                     System.out.printf("Your rating: %s%n", user.getScore());
                     break;
                 default:
-                    if (!isInputValid(userInput)) {
+                    if (!isInputValid(allChoices, userInput)) {
                         continue;
                     }
-                    checkWinner(userInput, user);
+                    finalChoices = optionsWithoutChoice(allChoices, userInput);
+                    checkWinner(allChoices, finalChoices, userInput, user);
             }
         }
     }
--- src/rockpaperscissors/User.java
+++ src/rockpaperscissors/User.java
@@ -1,12 +1,18 @@
 package rockpaperscissors;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class User {
+    private static final String SCORE_FILE_NAME = "rating.txt";
     private final String name;
     private int score;
 
     User(String name) {
         this.name = name;
-        this.score = 0;
+        loadScore();
+
     }
 
     public String getName() {
@@ -17,10 +23,51 @@ class User {
         return score;
     }
 
-    public void setScore(int score) {
+    private void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * Create file object using a possible location on the disk
+     * @return File object
+     */
+    private File readFile() {
+        return new File(SCORE_FILE_NAME);
+    }
+
+    private void loadScore() {
+        try {
+            setScoreFromFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found" + e.getMessage());
+            this.score = 0;
+        }
+    }
+
+    /**
+     * Try to load the User score from file, if it exists, if not set default to 0
+     * @throws FileNotFoundException If file not found, throw exception and set default
+     */
+    private void setScoreFromFile() throws FileNotFoundException {
+        try (Scanner sc = new Scanner(readFile())) {
+            while (sc.hasNext()) {
+                String[] userData = sc.nextLine().split(" ");
+                String playerName = userData[0];
+                int playerScore = Integer.parseInt(userData[1]);
+
+                if (this.getName().equals(playerName)) {
+                    this.setScore(playerScore);
+                    return;
+                }
+            }
+        }
+        this.score = 0;
+    }
+
+    /**
+     * Increase score
+     * @param score amount to increase the score with
+     */
     public void increaseScore(int score) {
         this.score += score;
     }
